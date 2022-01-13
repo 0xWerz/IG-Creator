3eeab2fe3a03af5759158f42fa0411d374742f6a
razewerz <78509039+razewerz@users.noreply.github.com>
2022-01-13 02:58:06 +0100

    Update fakeMail.py

--- fakeMail.py
+++ fakeMail.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 def getFakeMail():
-    url = 'https://email-fake.com/'
+    url = 'https://10minutemail.net/'
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
-    mail = soup.find_all("span", {"id": "email_ch_text"})
-    return mail[0].contents
+    mail = soup.find("button", {"id": "copy-button"})
+    return mail['data-clipboard-text']
+
+
